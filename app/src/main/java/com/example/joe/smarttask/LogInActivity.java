474fd69fba311d459f3ba1182052beb9e619ac08
joe <jburst16@student.aau.dk>
2017-03-14 10:47:19 +0100

    firebase json file added and logo

--- app/src/main/java/com/example/joe/smarttask/LogInActivity.java
+++ app/src/main/java/com/example/joe/smarttask/LogInActivity.java
@@ -8,8 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.joe.smarttask.MainPage.SmartTask_Main_Activity;
 import com.example.joe.smarttask.SignUp.SignUp;
+import com.example.joe.smarttask.SmartTask.SmartTask_Main_Activity;
 import com.example.joe.smarttask.Welcome.ShowWelcome;
 import com.example.joe.smarttask.Welcome.WelcomeActivity;
 
--- app/src/main/java/com/example/joe/smarttask/SmartTask/SmartTask_Main_Activity.java
+++ app/src/main/java/com/example/joe/smarttask/SmartTask/SmartTask_Main_Activity.java
@@ -1,4 +1,4 @@
-package com.example.joe.smarttask.MainPage;
+package com.example.joe.smarttask.SmartTask;
 
 import android.content.Intent;
 import android.os.Bundle;
