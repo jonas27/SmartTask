1fac7abdc83e17db931dd18637016c1e873e753d
joe <jburst16@student.aau.dk>
2017-03-14 13:22:20 +0100

    fix username

--- app/src/main/java/com/example/joe/smarttask/SmartTask/SmartTask_Main_Activity.java
+++ app/src/main/java/com/example/joe/smarttask/SmartTask/SmartTask_Main_Activity.java
@@ -1,22 +1,31 @@
 package com.example.joe.smarttask.SmartTask;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+import com.example.joe.smarttask.R;
 
 /**
  * Delete class --> naming is shit
  */
 
-public class SmartTask_Main_Activity extends AppCompatActivity {
+public class SmartTask_Main_Activity extends FragmentActivity {
 
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        Intent intent = getIntent();
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //set's the content (layout)
-        //setContentView(R.layout.intro_view_menu);
+        setContentView(R.layout.activity_smarttask);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = new SmartTask_Tasks_Fragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 
 
--- app/src/main/java/com/example/joe/smarttask/SmartTask/SmartTask_Tasks_Fragment.java
+++ app/src/main/java/com/example/joe/smarttask/SmartTask/SmartTask_Tasks_Fragment.java
@@ -1,12 +1,12 @@
 package com.example.joe.smarttask.SmartTask;
 
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.Fragment;
 
 /**
  * Created by joe on 14/03/2017.
  */
 
-public class SmartTask_Tasks_Fragment extends FragmentActivity {
+public class SmartTask_Tasks_Fragment extends Fragment {
 
 
 }
