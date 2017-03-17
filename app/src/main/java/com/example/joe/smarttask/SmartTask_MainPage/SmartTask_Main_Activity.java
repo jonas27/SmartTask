a4cc8db4fa7a62fed358ad28270a7a20d56c53e0
joe <jburst16@student.aau.dk>
2017-03-17 21:23:18 +0100

    Firebase integration as a singleton.

--- app/src/main/java/com/example/joe/smarttask/SmartTask_MainPage/SmartTask_Main_Activity.java
+++ app/src/main/java/com/example/joe/smarttask/SmartTask_MainPage/SmartTask_Main_Activity.java
@@ -2,22 +2,15 @@ package com.example.joe.smarttask.SmartTask_MainPage;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.joe.smarttask.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 /**
  * Delete class --> naming is shit
@@ -34,6 +27,8 @@ public class SmartTask_Main_Activity extends FragmentActivity {
     private FirebaseUser user;
     // [End declare Firebase auth]
 
+    private SmartTask_FireBase smartTask_fireBase;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Intent intent = getIntent();
@@ -41,46 +36,9 @@ public class SmartTask_Main_Activity extends FragmentActivity {
         //set's the content (layout)
         setContentView(R.layout.activity_smarttask);
 
-        // initialize_auth
-        mAuth = FirebaseAuth.getInstance();
 
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    // User is signed out
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
-                }
-                // ...
-            }
-        };
-
-
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference(mAuth.getCurrentUser().getUid());
-
-
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
+        // All firebase related change run over this singleton. (--> Calling it doesn't waste resources!)
+        smartTask_fireBase = SmartTask_FireBase.fireBase(this);
 
 
         //assign button to upload to firebase
@@ -88,13 +46,7 @@ public class SmartTask_Main_Activity extends FragmentActivity {
         upload.setOnClickListener(new View.OnClickListener() {
                                       @Override
                                       public void onClick(View v) {
-
-                                          // Write a message to the database
-                                          FirebaseDatabase database = FirebaseDatabase.getInstance();
-                                          DatabaseReference myRef = database.getReference(mAuth.getCurrentUser().getUid());
-
-
-                                          myRef.setValue("hello");
+                                          smartTask_fireBase.push();
                                       }
                                   }
         );
