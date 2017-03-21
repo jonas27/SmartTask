8b67bf062d12b6bd3b459d2d96993f3885d359ce
joe <jburst16@student.aau.dk>
2017-03-21 13:01:39 +0100

    mini changes

--- app/src/main/java/com/example/joe/smarttask/SmartTask_MainPage/SmartTask_FireBase.java
+++ app/src/main/java/com/example/joe/smarttask/SmartTask_MainPage/SmartTask_FireBase.java
@@ -1,19 +1,13 @@
 package com.example.joe.smarttask.SmartTask_MainPage;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -54,6 +48,7 @@ public class SmartTask_FireBase extends AppCompatActivity {
         }
         return smartTask_fireBase;
     }
+
 
     protected void addNewTask() {
         
--- app/src/main/java/com/example/joe/smarttask/SmartTask_MainPage/SmartTask_ListTasks_Fragment.java
+++ app/src/main/java/com/example/joe/smarttask/SmartTask_MainPage/SmartTask_ListTasks_Fragment.java
@@ -2,6 +2,8 @@ package com.example.joe.smarttask.SmartTask_MainPage;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,9 @@ import com.example.joe.smarttask.R;
 
 public class SmartTask_ListTasks_Fragment extends Fragment {
 
+    private RecyclerView mSmartTaskListTaskRecyclerView;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +27,11 @@ public class SmartTask_ListTasks_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_listtasks_smarttask, container, false);
-        return v;
+        View view = inflater.inflate(R.layout.fragment_listtasks_smarttask, container, false);
+
+        mSmartTaskListTaskRecyclerView = (RecyclerView) view.findViewById(R.id.smarttask_listtask_recycler_view);
+        mSmartTaskListTaskRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
     }
 
 
