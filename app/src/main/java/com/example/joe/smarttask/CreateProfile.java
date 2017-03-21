7da8b95de4340a7c7564c89a137115b1249ad02b
jgerts16 <jgerts16@student.aau.dk>
2017-03-21 14:09:30 +0100

    Creating Profile activity

--- app/src/main/java/com/example/joe/smarttask/CreateProfile.java
+++ app/src/main/java/com/example/joe/smarttask/CreateProfile.java
@@ -2,6 +2,8 @@ package com.example.joe.smarttask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class CreateProfile extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class CreateProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, PRIVILIGIES);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.Privilgies);
+        textView.setAdapter(adapter);
     }
+
+    private static final String[] PRIVILIGIES = new String[] {
+            "admin", "user", "child"
+    };
 }
+
+
