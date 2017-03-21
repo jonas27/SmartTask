8651f574c994a7ee025d6d59aa121886c78e87a9
jgerts16 <jgerts16@student.aau.dk>
2017-03-21 15:02:41 +0100

    Creating Profile activity

--- app/src/main/java/com/example/joe/smarttask/CreateProfile.java
+++ app/src/main/java/com/example/joe/smarttask/CreateProfile.java
@@ -25,4 +25,3 @@ public class CreateProfile extends AppCompatActivity {
 }
 
 
-//hw
--- app/src/main/java/com/example/joe/smarttask/SmartTask_MainPage/SmartTask_ListTasks_Fragment.java
+++ app/src/main/java/com/example/joe/smarttask/SmartTask_MainPage/SmartTask_ListTasks_Fragment.java
@@ -2,6 +2,7 @@ package com.example.joe.smarttask.SmartTask_MainPage;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -27,7 +28,7 @@ public class SmartTask_ListTasks_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_listtasks_smarttask, container, false);
-//
+
 //        mSmartTaskListTaskRecyclerView = (RecyclerView) view.findViewById(R.id.smarttask_listtask_recycler_view);
 //        mSmartTaskListTaskRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         return view;
