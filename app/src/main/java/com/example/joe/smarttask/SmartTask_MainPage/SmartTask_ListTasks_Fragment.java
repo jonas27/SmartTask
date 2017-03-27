35b9f742c9d169e7ad7dade0e0e23d3de07de719
joe <jburst16@student.aau.dk>
2017-03-27 10:06:21 +0200

    Small changes in main class

--- app/src/main/java/com/example/joe/smarttask/SmartTask_MainPage/SmartTask_ListTasks_Fragment.java
+++ app/src/main/java/com/example/joe/smarttask/SmartTask_MainPage/SmartTask_ListTasks_Fragment.java
@@ -2,7 +2,6 @@ package com.example.joe.smarttask.SmartTask_MainPage;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,7 +17,9 @@ public class SmartTask_ListTasks_Fragment extends Fragment {
 
     private RecyclerView mSmartTaskListTaskRecyclerView;
 
-
+    /* This Method should host nothing but super.onCreate method call as fragments follow a slightly different lifecycle than normal activities.
+       All intialisations and else should be in onCreateView
+    */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +30,13 @@ public class SmartTask_ListTasks_Fragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_listtasks_smarttask, container, false);
 
+        /*
+        *   TODO: Add recycler view to display list
+        * */
       //  mSmartTaskListTaskRecyclerView = (RecyclerView) view.findViewById(R.id.smarttask_listtask_recycler_view);
       //  mSmartTaskListTaskRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
         return view;
     }
 
