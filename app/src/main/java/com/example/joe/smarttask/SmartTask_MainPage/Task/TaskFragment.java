b675978f3adbf9f97d058be03ed0f4129451cd43
jojokbh <jkbh16@student.aau.dk>
2017-04-23 16:17:55 +0200

    Camera to branch out

--- app/src/main/java/com/example/joe/smarttask/SmartTask_MainPage/Task/TaskFragment.java
+++ app/src/main/java/com/example/joe/smarttask/SmartTask_MainPage/Task/TaskFragment.java
@@ -1,15 +1,13 @@
 package com.example.joe.smarttask.SmartTask_MainPage.Task;
 
 import android.content.Intent;
-import android.icu.text.DateFormat;
-import android.icu.text.SimpleDateFormat;
-import android.icu.util.Calendar;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.annotation.RequiresApi;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.FileProvider;
 import android.support.v7.widget.Toolbar;
@@ -18,13 +16,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.joe.smarttask.R;
+import com.example.joe.smarttask.SmartTask_MainPage.SingletonsAndSuperclasses.FireBase;
 import com.example.joe.smarttask.SmartTask_MainPage.List.ListTask;
 import com.example.joe.smarttask.SmartTask_MainPage.SMMainActivity;
-import com.example.joe.smarttask.SmartTask_MainPage.SingletonsAndSuperclasses.FireBase;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import org.w3c.dom.Text;
 
 import java.io.File;
 import java.io.IOException;
@@ -47,10 +56,9 @@ public class TaskFragment extends Fragment {
     private TaskObject mTask;
     private String mTaskId;
 
-//    [Start: Define views for task]
+    //    [Start: Define views for task]
     private TextView mTaskDate;
     private ImageView mTaskSolved;
-    private ImageView mTaskUnSolved;
     private Button mTaskEdit;
     private TextView mTaskName;
     private TextView mTaskResponsible;
@@ -64,10 +72,11 @@ public class TaskFragment extends Fragment {
     private Button mTaskConfirm;
     private ImageView mTaskImageView;
 //    [End: Define views for task]
-
+    private String file;
 
     private  ListTask mList;
-    private String dir = "/";
+    private String dir = "/storage/emulated/0/smarttask/";
+    private StorageReference storageRef;
 
     public static TaskFragment newInstance(String taskId) {
         Bundle args = new Bundle();
@@ -77,22 +86,10 @@ public class TaskFragment extends Fragment {
         return fragment;
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    private String getDate(long milliSeconds, String dateFormat)
-    {
-
-        // Create a DateFormatter object for displaying date in specified format.
-        DateFormat formatter = new SimpleDateFormat(dateFormat);
-
-        // Create a calendar object that will convert the date and time value in milliseconds to date.
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(Long.parseLong(mTask.getDatetime()));
-        return formatter.format(calendar.getTime());
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar_task);
 //        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
@@ -102,25 +99,18 @@ public class TaskFragment extends Fragment {
         this.mTask=mList.getTask(mTaskId);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_task, container, false);
 
-
-
-
         mTaskDate = (TextView) v.findViewById(R.id.task_date);
-        mTaskDate.setText(getDate(Long.parseLong(mTask.getDatetime()),"yyyy/MM/dd  hh:mm"));
-
-
+        mTaskDate.setText(mTask.getDatetime());
+        storageRef = FirebaseStorage.getInstance().getReference();
 
         mTaskSolved = (ImageView) v.findViewById(R.id.task_check);
-        mTaskUnSolved = (ImageView) v.findViewById(R.id.task_uncheck);
 
-        if(mTask.getStatus().toString().equals("true")){mTaskSolved.setVisibility(View.VISIBLE);mTaskUnSolved.setVisibility(View.INVISIBLE);
-        }else{mTaskSolved.setVisibility(View.INVISIBLE);mTaskUnSolved.setVisibility(View.VISIBLE);}
+        mTaskImageView = (ImageView) v.findViewById(R.id.task_imageview);
 
         mTaskEdit = (Button) v.findViewById(R.id.task_btn_edit);
 
@@ -131,18 +121,16 @@ public class TaskFragment extends Fragment {
         mTaskResponsible.setText(mTask.getResponsible());
 
         mTaskDescription = (TextView) v.findViewById(R.id.task_description);
-        mTaskDescription.setText(mTask.getDescription());
+        mTaskDescription.setText(mTask.getResponsible());
 
         mTaskCategory = (TextView) v.findViewById(R.id.task_category);
         mTaskCategory.setText(mTask.getCategories());
 
-        mTaskPriority = (TextView) v.findViewById(R.id.task_priority);
+        mTaskPriority = (TextView) v.findViewById(R.id.task_category);
         mTaskPriority.setText(mTask.getPriority());
 
         mTaskPoints =(TextView) v.findViewById(R.id.task_points);
         mTaskPoints.setText(mTask.getPoints());
-
-        mTaskImageView =(ImageView) v.findViewById(R.id.task_imageview);
 
 //        mTaskEdit = (Button) v.findViewById(R.id.task_btn_edit);
 //        mTaskEdit.setOnClickListener(new View.OnClickListener() {
@@ -163,24 +151,23 @@ public class TaskFragment extends Fragment {
                 f.createTask(mTask);
             }
         });
+        String[] perms = {"android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.CAMERA"};
 
+        int permsRequestCode = 200;
+
+        requestPermissions(perms, permsRequestCode);
         mTaskPicture = (Button) v.findViewById(R.id.task_btn_add_picture);
         mTaskPicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String[] perms = {"android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.CAMERA"};
 
-                int permsRequestCode = 200;
-
-                requestPermissions(perms, permsRequestCode);
-
-                String file = dir+mTaskId+".jpg";
+                file = dir+mTaskId+".jpg";
                 File newfile = new File(file);
-                //File folder = new File(dir);
-                //folder.mkdirs();
+                File folder = new File(dir);
+                folder.mkdirs();
                 try {
-
                     newfile.createNewFile();
+
                     Uri outputFileUri = FileProvider.getUriForFile(SMMainActivity.getAppContext(), SMMainActivity.getAppContext().getApplicationContext().getPackageName() + ".provider", newfile);
 
                     Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -196,6 +183,42 @@ public class TaskFragment extends Fragment {
             }
         });
 
+        File taskImage = new File(dir+mTaskId+".jpg");
+
+        if(taskImage.exists()){
+            Bitmap bitmap = BitmapFactory.decodeFile(taskImage.getAbsolutePath());
+            mTaskImageView.setImageBitmap(bitmap);
+        }else{
+            Log.d(TAG,"Getting from firebase");
+            StorageReference currentImage = storageRef.child("images/"+mTaskId+".jpg");
+
+            File localFile = null;
+            try {
+                localFile = new File(dir+mTaskId+".jpg");
+                localFile.createNewFile();
+                final File finalLocalFile = localFile;
+                currentImage.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        Log.d(TAG,"Picture exist");
+                        Bitmap bitmap = BitmapFactory.decodeFile(finalLocalFile.getAbsolutePath());
+                        mTaskImageView.setImageBitmap(bitmap);
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle any errors
+                        Log.d(TAG,"NO Picture exist");
+                    }
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
         return v;
     }
 
@@ -210,9 +233,33 @@ public class TaskFragment extends Fragment {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == 1 && resultCode == RESULT_OK) {
-            Uri u = data.getData();
+            Log.d(TAG,file);
+            Bitmap bitmap = BitmapFactory.decodeFile(file);
+            mTaskImageView.setImageBitmap(bitmap);
+            //Uri u = Uri.parse("file://"+file);
             Log.d(TAG, "Pic saved");
-            mTaskImageView.setImageURI(u);
+            //mTaskImageView.setImageURI(u);
+            Uri uriFile = Uri.fromFile(new File(file));
+            UploadTask uploadTask;
+
+            StorageReference fileUpload = storageRef.child("images/"+uriFile.getLastPathSegment());
+            uploadTask = fileUpload.putFile(uriFile);
+
+// Register observers to listen for when the download is done or if it fails
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                    Log.d(TAG,"failed");
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                    Log.d(TAG,"uploaded");
+                  //  Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                }
+            });
         }
     }
 }
